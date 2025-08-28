44e054264d8122d37e05027c4b5b642e5e96b937
piksi <pitelinaksenia83412@gmail.com>
2025-08-28 19:35:23 +0300

    create auth module

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(username: string, pass: string): Promise<any> {}
+}
